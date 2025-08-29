8267dd06b714dd8987358d68290404c14d173c52
MegeKaplan <kaplanmehmetege@gmail.com>
2025-08-29 17:45:03 +0300

    fix: add host 0.0.0.0 to app listen

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import { env } from './config/env.js'
 
 const startServer = async () => {
   try {
-    await app.listen({ port: Number(env.PORT) })
-    console.log(`Server is running on http://localhost:${env.PORT}`)
+    await app.listen({
+      port: Number(env.PORT),
+      host: "0.0.0.0"
+    })
+    console.log(`Server is running on http://localhost:${env.PORT}`);
   } catch (error) {
     console.error(error)
   }
