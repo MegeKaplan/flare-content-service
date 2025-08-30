02a4a398181da7c68e4eb64c41635b260a5d85dd
MegeKaplan <kaplanmehmetege@gmail.com>
2025-08-30 14:23:01 +0300

    feat: add CORS plugin

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import Fastify from 'fastify'
 import postsRoutes from './routes/posts.route.js'
 import mongoPlugin from './database/mongo.js'
+import cors from '@fastify/cors'
 
 const app = Fastify()
+
+await app.register(cors, {
+  origin: '*',
+  methods: ['*'],
+  allowedHeaders: ['*'],
+})
 
 await app.register(mongoPlugin)
 
